9678dd1f2225f4a07556731e2e0096732cd06055
Abinash Swain <117904767+abb14@users.noreply.github.com>
2023-01-28 19:53:27 +0530

    Add files via upload

---
+++ Student Result Management_Portal/src/student/result/management_portal/StudentResultManagement_Portal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student.result.management_portal;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class StudentResultManagement_Portal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
